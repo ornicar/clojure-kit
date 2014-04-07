c4a86f7f3e183d744e9b71fd24e2db75f48bc589
Sebastien Renault <sre@zenexity.com>
2014-04-07 23:01:55 +0200

    Support ref. Handle Authorisation errors.

--- src/io/prismic/api.clj
+++ src/io/prismic/api.clj
@@ -1,11 +1,37 @@
 (ns io.prismic.api
-  (:require [clj-http.client :as http]
-            [clojure.algo.generic.functor :as gf]))
+  (:use [clojure.core.match :only (match)])
+  (:require
+   [clj-http.client :as http]
+   [clojure.data.json :as json]
+   [clojure.algo.generic.functor :as gf]))
+
+(defn- authorization-needed[oauth-url]
+  (ex-info "You need to provide an access token to access this repository" {:type "AuthorizationNeeded" :oauth-url oauth-url}))
+
+(defn- invalid-token[oauth-url]
+  (ex-info "The provided access token is either invalid or expired" {:type "InvalidToken" :oauth-url oauth-url}))
+
+(defn- unexpected-error[msg]
+  (ex-info msg {:type "UnexpectedError"}))
 
 (defn get-api
-  ([url] (get-api url nil))
-  ([url token] (:body (http/get url {:query-params (if (nil? token) {} {:access_token token})
-                                     :accept :json :as :json}))))
+  ([url](get-api url nil))
+  ([url token]
+     (try
+       (let [params (filter #(not (nil? (second %))) {:access_token token :ref ref})]
+         (:body (http/get url {:query-params params :accept :json :as :json})))
+       (catch clojure.lang.ExceptionInfo e
+         (let [data (:object (ex-data e))
+               status (:status data)
+               body (json/read-str (get-in (ex-data e) [:object :body]))
+               oauth-initiate (get body "oauth_initiate")
+               error (get body "error")]
+
+           (match [status (nil? oauth-initiate) (nil? token)]
+                  [401 false false] (throw (invalid-token oauth-initiate))
+                  [401 _ true] (throw (authorization-needed oauth-initiate))
+                  [401 _ _] (throw (unexpected-error "Authorization error, but not URL was provided"))
+                  :else (throw (unexpected-error (str "Got an HTTP error " status ". " error)))))))))
 
 (defn get-form [api name] (-> api :forms name))
 (defn get-bookmark [api name] (-> api :bookmarks name))
@@ -14,10 +40,13 @@
 
 (defn- vects-to-map [vects] (apply merge (map #(hash-map (keyword (first %1)) (second %1)) vects)))
 
-(defn- search-no-cache [api form-name query]
+(defn- search-no-cache
+  ([api form-name query](search-no-cache api nil form-name query))
+  ([api ref form-name query]
   (let [form (get-form api form-name)
+        r (if (nil? ref) (:ref (get-ref api :Master)) ref)
         defaults (vects-to-map (filter (comp not nil? val) (gf/fmap #(:default %) (:fields form))))
-        params (merge {:ref (:ref (get-ref api :Master))} defaults query)
+        params (merge {:ref r} defaults query)
         response (:body (http/get (:action form) {:query-params params :accept :json :as :json}))
         add-fragments (fn [fragments [k v]]
                         (if (vector? v)
@@ -27,16 +56,21 @@
         parse-doc (fn [doc] (let [frags1 (get-in doc [:data (keyword (:type doc))])
                                   frags2 (reduce add-fragments {} frags1)]
                               (assoc (dissoc doc :data) :fragments frags2)))]
-    (assoc (dissoc response :results) :results (map parse-doc (:results response)))))
+    (assoc (dissoc response :results) :results (map parse-doc (:results response))))))
 
 (def search (memoize search-no-cache))
 
-(defn- get-by-id-no-cache [api id]
-  (first (:results (search api :everything {:q (str "[[:d = at(document.id, \"" id "\")]]")}))))
+(defn- get-by-id-no-cache
+  ([api id] (get-by-id-no-cache api nil id))
+  ([api ref id]
+  (first (:results (search api ref :everything {:q (str "[[:d = at(document.id, \"" id "\")]]")})))))
 
 (def get-by-id (memoize get-by-id-no-cache))
 
-(defn- get-by-bookmark-no-cache [api bookmark] (get-by-id api (get-bookmark api bookmark)))
+(defn- get-by-bookmark-no-cache
+  ([api bookmark](get-by-bookmark-no-cache api nil bookmark))
+  ([api ref bookmark]
+     (get-by-id api ref (get-bookmark api bookmark))))
 
 (def get-by-bookmark (memoize get-by-bookmark-no-cache))
 
