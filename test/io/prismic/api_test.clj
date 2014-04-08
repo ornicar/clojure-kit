dbaa03294a3702b108e91b2da3ecec352c326825
Sebastien Renault <sre@zenexity.com>
2014-04-08 17:00:51 +0200

    Add some tests about oauth exceptions

--- test/io/prismic/api_test.clj
+++ test/io/prismic/api_test.clj
@@ -4,11 +4,26 @@
 
 (def lbc (get-api "https://lesbonneschoses.prismic.io/api"))
 (def micro (get-api "https://micro.prismic.io/api"))
+(def test-endpoint "https://test.prismic.io/api")
+(def test
+  (let [token "MC5VMFB1N0FFQUFDNEEyZF82.77-9AFfvv73vv71D77-977-977-977-9Unk2au-_ve-_vU7vv73vv73vv70u77-977-977-977-9bO-_vQbvv70dKwY"]
+    (get-api test-endpoint token)))
+
 (defn- is= [a b] (is (= a b)))
 
 (deftest create
   (testing "get api"
-    (is= (:oauth_initiate lbc) "https://lesbonneschoses.prismic.io/auth")))
+    (is= (:oauth_initiate lbc) "https://lesbonneschoses.prismic.io/auth"))
+
+  (testing "get secured api"
+    (is= (:oauth_initiate test) "https://test.prismic.io/auth")))
+
+(deftest oauth-exceptions
+  (testing "invalid token"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"The provided access token is either invalid or expired" (get-api test-endpoint "abcd")))))
+
+  (testing "authorization needed"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"You need to provide an access token to access this repository" (get-api test-endpoint))))
 
 (deftest api-functions
 
