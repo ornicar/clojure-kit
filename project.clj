1f92ada5562997b1c9a60752a8f678a73af11956
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-08-01 11:29:36 +0200

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject prismic "1.0-M3"
+(defproject prismic "1.0"
   :description "Clojure kit for prismic.io API"
   :url "http://prismic.io"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]
                  [cheshire "5.3.1"]
-                 [org.clojure/data.json "0.2.1"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/core.match "0.2.1"]
                  [org.clojure/algo.generic "0.1.2"]
                  [org.clojure/core.cache "0.6.3"]])
